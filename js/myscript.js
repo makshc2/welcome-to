83477c2a6bae45aeff13d974711e18ad203348c2
makshc2 <makshc2@gmail.com>
2017-07-13 23:15:41 +0300

    grid-item

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,10 @@
-﻿
+﻿$(document).on('ready', function() {
+    $(".regular").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 3,
+
+    });
+
+});
